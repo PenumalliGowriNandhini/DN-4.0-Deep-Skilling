0712d3c6d561abe40d3d61e214cf2a83422ff66b
PenumalliGowriNandini <gowrinandhini988@gmail.com>
2025-06-19 19:08:26 +0530

    message

--- Week1_DesignPatternsAndPrinciples/02_FactoryPatterns/FactoryApp/Program.cs
+++ Week1_DesignPatternsAndPrinciples/02_FactoryPatterns/FactoryApp/Program.cs
@@ -1,84 +1,94 @@
 ﻿using System;
 
-// Step 1: Document Interface
-public interface IDocument
+namespace FactoryMethodPatternExample
 {
-    void Open();
-}
-
-// Step 2: Concrete Document Classes
-public class WordDocument : IDocument
-{
-    public void Open()
+    // Step 1 & 2: Document Interface
+    public interface IDocument
     {
-        Console.WriteLine("Opening a Word Document.");
+        void Open();
+    }
+
+    // Step 3: Concrete Document Classes
+    public class WordDocument : IDocument
+    {
+        public void Open()
+        {
+            Console.WriteLine("Opening Word Document.");
+        }
+    }
+
+    public class PdfDocument : IDocument
+    {
+        public void Open()
+        {
+            Console.WriteLine("Opening PDF Document.");
+        }
+    }
+
+    public class ExcelDocument : IDocument
+    {
+        public void Open()
+        {
+            Console.WriteLine("Opening Excel Document.");
+        }
+    }
+
+    // Step 4: Abstract Factory
+    public abstract class DocumentFactory
+    {
+        public abstract IDocument CreateDocument();
+    }
+
+    // Step 4: Concrete Factory Classes
+    public class WordDocumentFactory : DocumentFactory
+    {
+        public override IDocument CreateDocument()
+        {
+            return new WordDocument();
+        }
+    }
+
+    public class PdfDocumentFactory : DocumentFactory
+    {
+        public override IDocument CreateDocument()
+        {
+            return new PdfDocument();
+        }
+    }
+
+    public class ExcelDocumentFactory : DocumentFactory
+    {
+        public override IDocument CreateDocument()
+        {
+            return new ExcelDocument();
+        }
+    }
+
+    // Step 5: Test the Factory Method Implementation
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter document type to open (word/pdf/excel): ");
+            string docType = Console.ReadLine()?.Trim().ToLower();
+
+            DocumentFactory factory = docType switch
+            {
+                "word" => new WordDocumentFactory(),
+                "pdf" => new PdfDocumentFactory(),
+                "excel" => new ExcelDocumentFactory(),
+                _ => null
+            };
+
+            if (factory != null)
+            {
+                IDocument document = factory.CreateDocument();
+                document.Open();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter 'word', 'pdf', or 'excel'.");
+            }
+        }
     }
 }
-
-public class PdfDocument : IDocument
-{
-    public void Open()
-    {
-        Console.WriteLine("Opening a PDF Document.");
-    }
-}
-
-public class ExcelDocument : IDocument
-{
-    public void Open()
-    {
-        Console.WriteLine("Opening an Excel Document.");
-    }
-}
-
-// Step 3: Abstract Factory
-public abstract class DocumentFactory
-{
-    public abstract IDocument CreateDocument();
-}
-
-// Step 4: Concrete Factory Classes
-public class WordDocumentFactory : DocumentFactory
-{
-    public override IDocument CreateDocument()
-    {
-        return new WordDocument();
-    }
-}
-
-public class PdfDocumentFactory : DocumentFactory
-{
-    public override IDocument CreateDocument()
-    {
-        return new PdfDocument();
-    }
-}
-
-public class ExcelDocumentFactory : DocumentFactory
-{
-    public override IDocument CreateDocument()
-    {
-        return new ExcelDocument();
-    }
-}
-
-// Step 5: Test the Factory Method
-class Program
-{
-    static void Main(string[] args)
-    {
-        DocumentFactory wordFactory = new WordDocumentFactory();
-        IDocument word = wordFactory.CreateDocument();
-        word.Open();
-
-        DocumentFactory pdfFactory = new PdfDocumentFactory();
-        IDocument pdf = pdfFactory.CreateDocument();
-        pdf.Open();
-
-        DocumentFactory excelFactory = new ExcelDocumentFactory();
-        IDocument excel = excelFactory.CreateDocument();
-        excel.Open();
-    }
-}
-// See https://aka.ms/new-console-template for more information
-
--- Week1_DesignPatternsAndPrinciples/02_FactoryPatterns/FactoryApp/obj/Debug/net8.0/FactoryApp.AssemblyInfo.cs
+++ Week1_DesignPatternsAndPrinciples/02_FactoryPatterns/FactoryApp/obj/Debug/net8.0/FactoryApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FactoryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c8b4a10be3f8e1b80ad077f81552c155cf8b270")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+938ae8f824dec32d925ae6051619556fe7e4908d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FactoryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FactoryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
